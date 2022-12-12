f2201836fef9789e250bf74f4a40dff53026d0f5
Erik Aguayo <70922205+ErikAguayo@users.noreply.github.com>
2022-12-12 12:52:49 -0600

    Update AgenteReceptor.java

--- CLIPSJNI/AgenteReceptor.java
+++ CLIPSJNI/AgenteReceptor.java
@@ -1,46 +1,122 @@
-package examples.messaging;
- 
-import jade.core.*;
-import jade.core.behaviours.*;
+//Aguayo Urincho Erik Antonio
+//javac -cp lib\jade.jar;lib\CLIPSJNI.jar -d classes src\examples\doctor\*.java
+//java -cp lib\jade.jar;lib\CLIPSJNI.jar;classes jade.Boot -gui -agents resp0:examples.doctor.receiver;resp1:examples.doctor.receiver;resp2:examples.doctor.receiver
+
+Receiver
+
+package examples.doctor;
+import jade.core.Agent;
 import jade.lang.acl.ACLMessage;
- 
-public class AgenteReceptor extends Agent {
-   
-    protected void setup() {
-        addBehaviour(new ReceptorComportaminento());
-    }
+import jade.lang.acl.MessageTemplate;
+import jade.proto.ContractNetResponder;
+import jade.domain.FIPANames;
+import jade.domain.FIPAAgentManagement.NotUnderstoodException;
+import jade.domain.FIPAAgentManagement.RefuseException;
+import jade.domain.FIPAAgentManagement.FailureException;
+import net.sf.clipsrules.jni.*;
 
-   private class ReceptorComportaminento extends CyclicBehaviour {
-            private boolean fin = false;
-       
-            public void action() {
-                System.out.println(" Preparandose para recibir");
+public class receiver extends Agent {
+	private Environment clips;
 
-                ACLMessage mensaje = receive();
- 
-                if (mensaje!= null) {
-                    System.out.println(getLocalName() + ": acaba de recibir el siguiente mensaje: ");
-                    System.out.println(mensaje.toString());
+	public void infodoctor(){
+		System.out.println("loading knowledge base"); 
+                try{
+                    clips.build("(deftemplate disease(slot name)(multislot symptom)(multislot treatment))");
+                    clips.build("(deftemplate foundDisease (slot foundName))");
+					
+					
                    
+                    clips.build("(deffacts diseases"+
+                    	"(disease (name headache) (symptom \"luz encandescente\" \"visibilidad borrosa\" \"fatiga\" )"+
+                    	"(treatment \"descanso\" \"medicamento por doctor\" \"dejar pantallas\"))"+
+
+                    	"(disease (name temperature) (symptom \"cabeza caliente\" \"escalofrios\")"+
+                    	"(treatment \"descanso\" \"paños humedos\" \"dormir\" ))"+
+
+                    	"(disease (name flu) (symptom \"nariz congestionada\" \"estornudos seguidos\" \"tos\")"+
+                    	"(treatment \"antigripales\" \"descanso\" \"medicamentos controlados\" \"Antiinflamatorios\" \"Antibiotico\"))"+
+
+                    	"(disease (name covid) (symptom \"temperatura\" \"cuerpo cortado\" \"dificultad respiratoria\" \"dolor de garganta\")"+
+                    	"(treatment   \"paracetamol\" \"mucho descanso\" \"si es necesario ir a urgencias\"))"+
+
+                    	"(disease (name allergy) (symptom \"Comezon en la zona afectada\" \"hinchazon\"  \"asfixia\")"+
+                    	"(treatment \"Antiinflamatorios\" \"alejarse de la causa\" \"descanso\" \"no rascarse\")))");
+                    clips.reset();
+                } catch (Exception e) {
+                  e.printStackTrace();
                 }
+	}
 
 
-                 System.out.println("Invoking Tell"); 
-              clips.eval("(clear)");
-              clips.build("(defrule r1 (estornuda) (frio) => (printout  t "tiene resfriado" crlf) (assert (dolor de cabeza)) ");
-              clips.build("(defrule r2 (dolor de cabeza) (garganta irritada) => (printout  t "dolor en garganta" crlf) (assert (posible covid)) ");
-              clips.build("(defrule r3 (posible covid) (cuerpo cortado) => (printout  t "se realizo prueba covid" crlf) (assert (dio positivo)) ");
-              clips.build("(defrule r4 (dio positivo) => (printout  t "covid verificado" crlf) ");
-              clips.build("(defrule r5 (estornuda) (calor)=> (printout  t "tiene temperatura" crlf) ");
-              clips.eval("(reset)");
 
- 
-            //Obtiene el primer mensaje de la cola de mensajes
-                
-            }
+	protected void setup() {
+		
+        clips = new Environment(); 
 
-            public boolean done() {
-                return fin;
-            }
-    }
+		infodoctor();
+
+		System.out.println("Agent "+getLocalName()+" waiting for request");
+		
+		MessageTemplate template = MessageTemplate.and(
+		MessageTemplate.MatchProtocol(FIPANames.InteractionProtocol.FIPA_CONTRACT_NET),
+		MessageTemplate.MatchPerformative(ACLMessage.CFP) );
+		
+		addBehaviour(new ContractNetResponder(this, template) {
+			
+			@Override
+			protected ACLMessage handleCfp(ACLMessage cfp) throws NotUnderstoodException, RefuseException {
+				System.out.println("Agent "+getLocalName()+": CFP received from "+cfp.getSender().getName()+". Action is "+cfp.getContent());
+				ACLMessage propose = cfp.createReply();
+				
+				try{
+					clips.build("(defrule diagnosis (disease (name ?n) (symptom $? "+cfp.getContent()+" $?)) => (assert (foundDisease (foundName ?n) ) ))");
+					clips.run();
+					FactAddressValue fv = clips.findFact("foundDisease");
+					String disease = fv.getSlotValue("foundName").toString();
+					System.out.println("Agent "+getLocalName()+": Proposing "+disease);
+					
+					propose.setPerformative(ACLMessage.PROPOSE);
+					propose.setContent(String.valueOf(disease));
+					
+				}catch (Exception e) {
+					e.printStackTrace();
+				  }
+				return propose;
+				
+			}
+
+			@Override
+			protected ACLMessage handleAcceptProposal(ACLMessage cfp, ACLMessage propose,ACLMessage accept) throws FailureException {
+				System.out.println("Agent "+getLocalName()+": Proposal accepted");
+					
+					
+					ACLMessage inform = accept.createReply();
+				 	System.out.println("Agent "+getLocalName()+": Action successfully performed");
+					
+					 try{
+						
+						clips.build("(defrule treatment ?f <-(disease (name  "+propose.getContent()+")(treatment $?b )) => (assert (foundTreatment (fact-slot-value ?f treatment  ) )))");
+						clips.run();
+						//t=1;
+						FactAddressValue fv = clips.findFact("foundTreatment");
+						String treatment = fv.getSlotValue("implied").toString();
+						System.out.println("Agent "+getLocalName()+": Proposing "+treatment);
+						inform.setPerformative(ACLMessage.INFORM);
+						inform.setContent(String.valueOf(treatment));
+					}catch (Exception e) {
+						e.printStackTrace();
+				 	}
+
+					return inform;
+
+
+			}
+
+			protected void handleRejectProposal(ACLMessage cfp, ACLMessage propose, ACLMessage reject) {
+				System.out.println("Agent "+getLocalName()+": Proposal rejected");
+			}
+		} );
+	}
+
+	
 }
